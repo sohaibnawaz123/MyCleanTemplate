bfa0a69472f1ace5bf0846ff3be24df42f1a17c1
Bilal Faheem <bilal.faheem@mmcgbl.com>
2026-02-09 19:13:28 +0500

    - get_view import fixes

--- get_view/__brick__/lib/modules/{{module.snakeCase()}}/domain/usecase/{{name.snakeCase()}}_use_case.dart
+++ get_view/__brick__/lib/modules/{{module.snakeCase()}}/domain/usecase/{{name.snakeCase()}}_use_case.dart
@@ -1,5 +1,5 @@
 import 'package:fpdart/fpdart.dart';
-import 'package:{{app.snakeCase()}}/features/app/domain/entitties/base_entity.dart';
+import 'package:{{app.snakeCase()}}/modules/app/domain/entitties/base_entity.dart';
 import 'package:{{app.snakeCase()}}/modules/{{module.snakeCase()}}/domain/entities/{{name.snakeCase()}}_entity.dart';
 import 'package:{{app.snakeCase()}}/modules/{{module.snakeCase()}}/domain/failures/{{name.snakeCase()}}_failure.dart';
 import 'package:{{app.snakeCase()}}/modules/{{module.snakeCase()}}/domain/params/{{name.snakeCase()}}_param.dart';
